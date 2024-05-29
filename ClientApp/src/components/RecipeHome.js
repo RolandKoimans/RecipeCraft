1e09520c10b79f817b93b1e20a626d6ebf98aacd
Roland Koimans <roland.koimans@gmail.com>
2024-05-29 21:29:57 +0200

    Added a new page to display recipe information

---
+++ ClientApp/src/components/RecipeHome.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export class RecipeHome extends Component {
+  static displayName = RecipeHome.name;
+
+  render() {
+    return (
+      <div>
+        <h1>RecipeCraft</h1>
+
+            <p>There's gonna be information about recipes here... someday...</p>
+
+      </div>
+    );
+  }
+}
